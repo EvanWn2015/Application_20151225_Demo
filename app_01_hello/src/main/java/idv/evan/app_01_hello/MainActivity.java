59fc5d112f74f181849927d23e2065ed45646dac
EvanWn2015 <jnswcy@gmail.com>
2016-03-06 11:40:49 +0800

    Initial commit

---
+++ app_01_hello/src/main/java/idv/evan/app_01_hello/MainActivity.java
@@ -0,0 +1,13 @@
+package idv.evan.app_01_hello;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+// Hello Android 
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
